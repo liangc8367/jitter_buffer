a0a8af53bd98894fbb7508ea2c75333e15e3a990
Liang C <liangc@ospray.bluesky>
2015-01-14 23:19:50 -0500

    - initial version of JitterBuffer

--- src/com/bluesky/JitterBuffer.java
+++ src/com/bluesky/JitterBuffer.java
@@ -54,12 +54,12 @@ public class JitterBuffer<E> {
 
         /**
         * @param   o the object to be compared.
-                * @return  a negative integer, zero, or a positive integer as this object
+        * @return  a negative integer, zero, or a positive integer as this object
         *          is less than, equal to, or greater than the specified object.
         *
-                * @throws NullPointerException if the specified object is null
-                * @throws ClassCastException if the specified object's type prevents it
-                *         from being compared to this object.
+        * @throws NullPointerException if the specified object is null
+        * @throws ClassCastException if the specified object's type prevents it
+        *         from being compared to this object.
          */
         @Override
         public int compareTo(Delayed o) {
---
+++ src/test/com/bluesky/JitterBufferTest.java
@@ -0,0 +1,23 @@
+package test.com.bluesky;
+
+import junit.framework.TestCase;
+
+public class JitterBufferTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOffer() throws Exception {
+
+    }
+
+    public void testPoll() throws Exception {
+
+    }
+}
